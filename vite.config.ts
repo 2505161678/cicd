1703ecc977b0febe32f908222d053357f4a6a945
zhangxx <2505161678@qq.com>
2025-06-06 18:27:47 +0800

    🎨 base路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cicd/', // 设置 base 为仓库名
+  // base: '/cicd/', // 设置 base 为仓库名
+  base: './', // 设置 base 为仓库名
   plugins: [vue()],
 });
